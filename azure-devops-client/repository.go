b4c4ee0a8a0041814647394b04da0932a9e360c9
clayton555 <claytonhowe@gmail.com>
2024-08-19 10:52:04 +0100

    Commented out collectors we're not using Reduced how far we look back for builds to March 2024

--- azure-devops-client/repository.go
+++ azure-devops-client/repository.go
@@ -84,7 +84,7 @@ func (c *AzureDevopsClient) ListCommits(project string, repository string, fromD
 	c.concurrencyLock()
 
 	url := fmt.Sprintf(
-		"_apis/git/repositories/%s/commits?searchCriteria.fromDate=2024-01-01&api-version=%v&searchCriteria.$top=5000",
+		"_apis/git/repositories/%s/commits?searchCriteria.fromDate=2024-03-01&api-version=%v&searchCriteria.$top=5000",
 		url.QueryEscape(repository),
 		//url.QueryEscape(fromDate.Format(time.RFC3339)),
 		url.QueryEscape(c.ApiVersion),
